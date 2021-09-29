24c89440db0984306bd2a9d11609c73f14a6e981
Gabriella MARINSESILVA <gabriella.marins@Digital-Grenoble.local>
2021-09-29 17:05:23 +0200

    the form is ugly and doesn't work yet

--- vueflix/src/App.vue
+++ vueflix/src/App.vue
@@ -4,39 +4,93 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1> The Crazy Cat Movie Reviwer </h1>
     <br>
+    <h4>Last Rated Movies</h4>
+   <div v-for="movie in movies"  :key="movie">
+     <div class="mt-4">
 
-   <div v-for="movie in movies"  :key="movie" class="list">
-<!--    {{movie}}: {{value}}-->
-     <h2>  {{movie.id}} - {{movie.title}} </h2>
-
-
-     Genres:  <div v-for="genre in movie.genres" v-bind:key="genre.id">
-         {{genre}}
-   </div>
-     <br>
-     Rating: {{movie.rating}}
-     <br>
+       <b-card>
+         <b-card-text>
+           <h2>  {{movie.id}} - {{movie.title}} </h2>
+           Genres: <div v-for="genre in movie.genres" v-bind:key="genre.id">
+            {{genre}}
+                   </div>
+           <br>
+           Rating: {{movie.rating}}
+         </b-card-text>
+       </b-card>
      <br>
     </div>
     <br>
+   </div>
     <h3> Total of Movies in this list: {{movies.length}}</h3>
 <br>
-<div>
-    <h4>List boostrapVue</h4>
-    <b-card img-src="" img-alt="Card image" img-left class="mb-3">
-      <b-card-text>
-        Some quick example text to build on the card and make up the bulk of the card's content.
-      </b-card-text>
-    </b-card>
 
-    <b-card img-src="" img-alt="Card image" img-right>
-      <b-card-text>
-        Some quick example text to build on the card and make up the bulk of the card's content.
-      </b-card-text>
-    </b-card>
-  </div>
-  </div>
+    <h2>Add a movie to the list</h2>
+    <div id="form_add_movie">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group
+            id="input-group-1"
+            label="Title:"
+            label-for="input-1"
+            description="We'll never share your email with anyone else."
+        >
+          <b-form-input
+              id="input-1"
+              v-model="movies.id"
+              type=""
+              placeholder="ID"
+              required
+          ></b-form-input>
 
+
+          <b-form-input
+              id="input-1"
+              v-model="movies.title"
+              type="text"
+              placeholder="What is the movie's name?"
+              required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+          <b-form-checkbox-group
+              v-model="movies.genres"
+              id="checkboxes-4"
+              :aria-describedby="ariaDescribedby"
+          >
+            <b-form-checkbox value="me">Comedy</b-form-checkbox>
+            <b-form-checkbox value="that">Drama</b-form-checkbox>
+            <b-form-checkbox value="that">Thriller</b-form-checkbox>
+            <b-form-checkbox value="that">Horror</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+          <b-form-input
+              id="input-2"
+              v-model="movies.review"
+              placeholder="Tell us your impressions?"
+              required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+          <b-form-input
+              id="input-2"
+              v-model="movies.description"
+              placeholder="Enter name"
+              required
+          ></b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </div>
+
+
+
+
+</div>
 </template>
 
 <script>
@@ -51,17 +105,18 @@ export default {
 
   data: function () {
     return {
- //     title: "Bienvenue sur VueFlix !",
+      //     title: "Bienvenue sur VueFlix !",
       // On peut rajouter nos films ici
       movies: [
-          {
-      id: 1,
-      title: "Parasite",
-      genres: ["comedy", "drama", "thriller"],
-      rating: 9,
-      review: "With an insightful and searing exploration of human behavior, ‘Parasite’ is a masterfully crafted film that is a definite must watch.",
-      description: "Parasite (Korean: 기생충; RR: Gisaengchoong) is a 2019 South Korean black comedy thriller film directed by Bong Joon-ho, who also co-wrote the screenplay with Han Jin-won."
-    },
+        {
+          id: 1,
+          title: "Parasite",
+          genres: ["comedy", "drama", "thriller"],
+          rating: 9,
+          review: "With an insightful and searing exploration of human behavior, ‘Parasite’ is a masterfully crafted film that is a definite must watch.",
+          description: "Parasite (Korean: 기생충; RR: Gisaengchoong) is a 2019 South Korean black comedy thriller film directed by Bong Joon-ho, who also co-wrote the screenplay with Han Jin-won.",
+
+        },
         {
           id: 2,
           title: "Boyhood",
@@ -78,10 +133,46 @@ export default {
           review: "With an insightful and searing exploration of human behavior, ‘Parasite’ is a masterfully crafted film that is a definite must watch.",
           description: "Parasite (Korean: 기생충; RR: Gisaengchoong) is a 2019 South Korean black comedy thriller film directed by Bong Joon-ho, who also co-wrote the screenplay with Han Jin-won."
         },
-    ]
-    };
+      ],
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+      show: true
+
+    }
   },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset(event) {
+      event.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+
+  }
+
+
 }
+
+
+
+
+
 </script>
 
 <style>
