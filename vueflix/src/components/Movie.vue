42c901cbb44799012caea163f13cbafe3113099e
Gabriella MARINSESILVA <gabriella.marins@Digital-Grenoble.local>
2021-09-30 17:17:19 +0200

    the filter is a drama

---
+++ vueflix/src/components/Movie.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+
+  <h1>{{id}} - {{title}}</h1>
+  <li>Genres: <div v-for="genre in genres" v-bind:key="genre.id">
+    {{genre}} </div></li>
+      <li>Rate: {{rating}} </li>
+  <br>
+     </div>
+    </template>
+
+    <script>
+    export default {
+      name: "Movie",
+      props: {
+        "id" : Number,
+        "title": String,
+        "genres": Array,
+        "rating": Number,
+        "description": String,
+        "review": String,
+
+      }
+    }
+    </script>
+
+    <style scoped>
+
+    </style>
